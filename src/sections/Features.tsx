5ce97dc9b14df23c99646ab125a16d64e0a67ba8
Farhan Ahmed <farhanf7n@gmail.com>
2024-11-30 13:56:23 +0500

    Added animations to 2 cards

--- src/sections/Features.tsx
+++ src/sections/Features.tsx
@@ -3,6 +3,7 @@ import Tag from "@/components/Tags";
 import avatar1 from "@/assets/images/avatar-ashwin-santiago.jpg";
 import avatar2 from "@/assets/images/avatar-lula-meyers.jpg";
 import avatar3 from "@/assets/images/avatar-florence-shaw.jpg";
+import avatar4 from "@/assets/images/avatar-owen-garcia.jpg";
 import Image from "next/image";
 import Avatar from "@/components/Avatar";
 import Key from "@/components/Key";
@@ -32,7 +33,7 @@ export default function Features() {
                     <FeatureCard
                         title="Real-time Collaboration"
                         description="Work Together seemlessly with conflict-free team editing"
-                        className="md:col-span-2 lg:col-span-1"
+                        className="md:col-span-2 lg:col-span-1 group"
                     >
                         <div className="aspect-video flex items-center justify-center gap-2">
                             <Avatar className="z-40">
@@ -56,8 +57,13 @@ export default function Features() {
                                     alt="Team member 3"
                                 />
                             </Avatar>
-                            <Avatar className="-ml-6 border-transparent">
-                                <div className="size-full bg-neutral-700 rounded-full inline-flex items-center justify-center gap-1">
+                            <Avatar className="-ml-6 border-transparent group-hover:border-green-500 transition">
+                                <div className="relative size-full bg-neutral-700 rounded-full inline-flex items-center justify-center gap-1">
+                                    <Image
+                                        src={avatar4}
+                                        alt="Team member 4"
+                                        className="absolute size-full rounded-full opacity-0 group-hover:opacity-100 transition"
+                                    ></Image>
                                     {Array.from({ length: 3 }).map((_, i) => (
                                         <span
                                             className="size-1.5 rounded-full bg-white inline-flex"
@@ -71,13 +77,21 @@ export default function Features() {
                     <FeatureCard
                         title="Interactive Prototyping"
                         description="Enagege your clients with prototypes that react to user actions"
-                        className="md:col-span-2 lg:col-span-1"
+                        className="md:col-span-2 lg:col-span-1 group"
                     >
                         <div className="aspect-vedio flex items-center justify-center">
-                            <p className="text-4xl font-extrabold text-white/20 text-center">
+                            <p className="text-4xl font-extrabold text-white/20 group-hover:text-white/10 transition duration-500 text-center">
                                 We&apos;ve achived{" "}
-                                <span className="bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent">
-                                    incredible
+                                <span className="bg-gradient-to-r from-purple-400 to-pink-400 bg-clip-text text-transparent relative">
+                                    <span>incredible</span>
+                                    <video
+                                        src="/assets/gif-incredible.mp4"
+                                        autoPlay
+                                        loop
+                                        muted
+                                        playsInline
+                                        className="absolute bottom-full left-1/2 -translate-x-1/2 rounded-2xl shadow-xl opacity-0 pointer-events-none transition duration-500 group-hover:opacity-100"
+                                    ></video>
                                 </span>{" "}
                                 growth this year
                             </p>
